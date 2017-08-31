19081fbe00c3975ec5c610d40a128668be0b6278
Morioka Shuhei <selmertsx@gmail.com>
2017-08-31 09:26:41 +0900

    add annotate to user and couple

--- app/models/couple.rb
+++ app/models/couple.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: couples
+#
+#  id         :integer          not null, primary key
+#  partner_1  :integer          not null
+#  partner_2  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Couple < ApplicationRecord
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(70)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
 end
