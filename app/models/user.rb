21d873b2af2cafbfffd0052f61304cf05f51c90b
Morioka Shuhei <selmertsx@gmail.com>
2017-09-06 09:07:19 +0900

    add email validates to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,11 @@
 #
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 70 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
   has_secure_password
 end
